70978b3b3deeed9bcebef258f79d2a39a8818f3c
Mike <mobrienil78@gmail.com>
2017-01-08 08:09:42 -0600

    add Jasmine

---
+++ NgTest/js/spec/spec/appSpec.js
@@ -0,0 +1,24 @@
+﻿describe("Controller", function () {
+    it("starts with no active tab", function () {
+        var ctrl = new SomeController();
+        expect(ctrl.activeTab).toEqual("");
+    });
+
+    it("sets tab 1 active when user hits confirm", function () {
+        var ctrl = new SomeController();
+        stubConfirmToReturn(true);
+        ctrl.tab1Click();
+        expect(ctrl.activeTab).toEqual("tab1");
+    });
+
+    it("cancels tab 1 navigation if user hits cancel", function () {
+        var ctrl = new SomeController();
+        stubConfirmToReturn(false);
+        ctrl.tab1Click();
+        expect(ctrl.activeTab).toEqual("");
+    });
+
+    function stubConfirmToReturn(val) {
+        window.confirm = function () { return val; }
+    }
+});
---
+++ NgTest/js/src/app.js
@@ -0,0 +1,30 @@
+﻿var myapp = angular.module('myapp', [])
+  .controller('SomeController', SomeController);
+
+function SomeController () {
+
+      this.message = 'Click a tab';
+      this.activeTab = '';
+
+      this.tab1Click = function () {
+          if (confirm('really?!')) {
+              this.message = 'Tab 1 Clicked';
+              this.activeTab = 'tab1';
+          }
+      };
+
+      this.getTab1CssClasses = function () {
+          return this.activeTab === 'tab1' ? 'active' : null;
+      };
+
+      this.tab2Click = function () {
+          if (confirm('really?!')) {
+              this.message = 'Tab 2 Clicked';
+              this.activeTab = 'tab2';
+          }
+      };
+
+      this.getTab2CssClasses = function () {
+          return this.activeTab === 'tab2' ? 'active' : null;
+      };
+  }
